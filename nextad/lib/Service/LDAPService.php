d8fb16f99da179a0ef88adc143a4b29a0f02fbcb
Mario Oliva <90973494+Alkyz@users.noreply.github.com>
2023-11-15 16:37:35 -0500

    Update LDAPService.php

--- nextad/lib/Service/LDAPService.php
+++ nextad/lib/Service/LDAPService.php
@@ -7,26 +7,6 @@ use OCP\LDAP\ILDAPProviderFactory;
 
 class LDAPService {
 
-    // private $ldapProvider;
-    // private $l10n;
-
-    // public function __construct(ILDAPProvider $ldapProvider, IL10N $l10n) {
-    //     $this->ldapProvider = $ldapProvider;
-    //     $this->l10n = $l10n;
-    // }
-
-    // public function connect() {
-    //     $uid = 'test.user1';
-
-    //     $ldapConnection = $this->ldapProvider->getLDAPConnection($uid);
-
-    //     if($ldapConnection->isConnected()) {
-    //         return $this->l10n->t('Connected to LDAP server');
-    //     } else {
-    //         throw new \Exception($this->l10n->t('Could not connect to LDAP server'));
-    //     }
-    // }
-
     private $ldapProviderFactory;
     private $l10n;
 
@@ -36,11 +16,9 @@ class LDAPService {
     }
 
     public function connect() {
-        // Get the LDAPProvider from the factory when it's needed, not in the constructor
         $ldapProvider = $this->ldapProviderFactory->getLDAPProvider();
 
-        // Use the LDAPProvider to establish a connection
-        // This assumes you have a method to get a connection configured for a specific user
+        // Get User UID from your AD server and pass it into getLDAPConnection()
         $ldapConnection = $ldapProvider->getLDAPConnection();
 
         if($ldapConnection->isConnected()) {
