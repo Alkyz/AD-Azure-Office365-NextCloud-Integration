8c22f07e25aa7825b3823e930a3059ae60108dc2
Mario Oliva <90973494+Alkyz@users.noreply.github.com>
2023-11-15 13:50:58 -0500

    Update Application.php to register new services

--- nextad/lib/appinfo/Application.php
+++ nextad/lib/appinfo/Application.php
@@ -1,16 +1,38 @@
 <?php
-declare(strict_types=1);
-// SPDX-FileCopyrightText: Mario Oliva <moliv149@fiu.edu>
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
 namespace OCA\NextAD\AppInfo;
 
 use OCP\AppFramework\App;
+use OCP\AppFramework\IAppContainer;
+use OCA\NextAD\Service\LDAPService;
+use OCA\NextAD\Controller\PageController;
+use OCP\IL10N;
+use OCP\LDAP\ILDAPProvider;
+use OCP\LDAP\ILDAPProviderFactory;
+use OCP\IRequest;
 
 class Application extends App {
-	public const APP_ID = 'nextad';
+    public const APP_ID = 'nextad';
 
-	public function __construct() {
-		parent::__construct(self::APP_ID);
-	}
+    public function __construct(array $urlParams = []) {
+        parent::__construct(self::APP_ID, $urlParams);
+        $this->registerServices();
+    }
+
+    private function registerServices() {
+        $container = $this->getContainer();
+	    
+	$container->registerService(LDAPService::class, function(IAppContainer $c) {
+		return new LDAPService(
+			$c->query(ILDAPProviderFactory::class),
+			$c->query(IL10N::class)
+		);
+	});
+
+        $container->registerService(PageController::class, function(IAppContainer $c) {
+            return new PageController(
+                $c->query(IRequest::class),
+                $c->query('LDAPService')
+            );
+        });
+    }
 }
