583aa9134560a18a28cac5c1450b25602a942674
Mario Oliva <90973494+Alkyz@users.noreply.github.com>
2023-11-17 10:18:35 -0500

    Update main.php, new display

--- nextad/templates/main.php
+++ nextad/templates/main.php
@@ -6,38 +6,39 @@ declare(strict_types=1);
 <?php
 ?>
 <div id="ldap-user-settings">
-    <h1>Customize LDAP User Settings</h1>
+    <h1>Customize Active Directory User Settings</h1>
     <p>Note: Changed user settings will be reflected as part of the user's newest Active Directory attributes.</p>
     <form id="user-settings-form">
         <div class="form-row">
             <label for="display-name">Display Name:</label>
-            <input type="text" id="display-name" name="display_name">
+            <input type="text" id="display-name" name="displayName">
         </div>
         <div class="form-row">
             <label for="email">Email:</label>
             <input type="email" id="email" name="email">
         </div>
         <div class="form-row">
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password">
+            <label for="company">Company:</label>
+            <input type="text" id="company" name="company">
         </div>
         <div class="form-row">
             <label for="telephone">Telephone:</label>
             <input type="tel" id="telephone" name="telephone">
         </div>
         <div class="form-row">
-            <label for="address">Address:</label>
-            <input type="text" id="address" name="address">
+            <label for="address">Street Address:</label>
+            <input type="text" id="address" name="streetAddress">
         </div>
         <div class="form-row">
             <label for="web-page">Web Page:</label>
-            <input type="url" id="web-page" name="web_page">
+            <input type="url" id="web-page" name="webPage">
         </div>
         <div class="form-row">
             <label for="description">Description:</label>
             <textarea id="description" name="description"></textarea>
         </div>
-        <div class="form-row">
+        <div class="form-row button-row">
+            <button id="preview-button" type="button">Preview Attributes</button>
             <button id="submit-button" type="submit">Set As Default</button>
         </div>
     </form>
