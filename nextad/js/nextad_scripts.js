3438c22bee676de5a88be167c9dd94724b132a69
Mario Oliva <90973494+Alkyz@users.noreply.github.com>
2023-11-20 13:04:57 -0500

    Update nextad_scripts.js to use new callback function logic

--- nextad/js/nextad_scripts.js
+++ nextad/js/nextad_scripts.js
@@ -1,6 +1,10 @@
+// Fetch uid logic here (i.e let userId = ...)
+
 document.addEventListener('DOMContentLoaded', function () {
     var previewButton = document.getElementById('preview-button');
     var submitButton = document.getElementById('submit-button');
+    var wipeButton = document.getElementById('wipe-button');
+    var form = document.getElementById('user-settings-form');
 
     function toggleInputFields(disabled, data = null) {
         var formElements = document.querySelectorAll('#user-settings-form input, #user-settings-form textarea');
@@ -9,16 +13,39 @@ document.addEventListener('DOMContentLoaded', function () {
             if (data) {
                 var name = element.name;
                 element.value = data['userData'][name];
-            } else if (disabled === false) {
-                element.value = '';
-            }
+            } 
         });
     }
 
+    function showMessage(message, color) {
+        var messageDiv = document.createElement('div');
+        messageDiv.classList.add('alert-message');
+        messageDiv.textContent = message;
+        messageDiv.style.color = color;
+        messageDiv.style.borderColor = color;
+        messageDiv.style.backgroundColor = color === '#8B0000' ? '#ffcccc' : '#ccffcc';
+
+        var container = document.getElementById('ldap-user-settings');
+        container.appendChild(messageDiv);
+
+        setTimeout(() => {
+            container.removeChild(messageDiv);
+        }, 5000);
+    }
+
+    wipeButton.addEventListener('click', function () {
+        var formElements = document.querySelectorAll('#user-settings-form input, #user-settings-form textarea');
+        formElements.forEach(function (element) {
+            element.value = '';
+        });
+    })
+
     previewButton.addEventListener('click', function () {
+
         if (previewButton.textContent === 'Preview Attributes') {
+            wipeButton.disabled = true;
             submitButton.disabled = true;
-            fetch('/index.php/apps/nextad/getUserAttributes')
+            fetch('/index.php/apps/nextad/getUserAttributes/' + userId)
                 .then(response => response.json())
                 .then(data => {
                     console.log(data.debug);
@@ -27,9 +54,11 @@ document.addEventListener('DOMContentLoaded', function () {
                 })
                 .catch(error => {
                     console.error('Error:', error);
+                    wipeButton.disabled = false;
                     submitButton.disabled = false;
                 });
         } else {
+            wipeButton.disabled = false;
             submitButton.disabled = false;
             toggleInputFields(false);
             previewButton.textContent = 'Preview Attributes';
@@ -40,20 +69,39 @@ document.addEventListener('DOMContentLoaded', function () {
     submitButton.addEventListener('click', function (event) {
         event.preventDefault();
 
-        var displayName = document.getElementById('display-name').value;
-        var email = document.getElementById('email').value;
-        var company = document.getElementById('company').value;
-        var telephone = document.getElementById('telephone').value;
-        var address = document.getElementById('address').value;
-        var webpage = document.getElementById('web-page').value;
-        var description = document.getElementById('description').value;
+        var formData = new FormData(form);
+        formData.append('uid', userId);
 
-        console.log('Display Name:', displayName);
-        console.log('Email:', email);
-        console.log('Company:', company);
-        console.log('Telephone:', telephone);
-        console.log('Address:', address);
-        console.log('Web Page:', webpage);
-        console.log('Description:', description);
+        var object = {};
+        var isFieldFilled = false;
+
+        formData.forEach((value, key) => {
+            object[key] = value;
+            if (value.trim() !== '' && key !== 'uid') {
+                isFieldFilled = true;
+            }
+        });
+
+        if (!isFieldFilled) {
+            showMessage("Please Update a Setting", '#8B0000');
+            return;
+        }
+
+        var json = JSON.stringify(object);
+        console.log(json);
+
+        fetch('/index.php/apps/nextad/putUserAttributes/' + userId, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: json
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            console.log("Debug:", data.debug);
+            showMessage("Successfully Updated AD Settings", 'green');
+        });
     });
 });
