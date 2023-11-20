407f8109ac9ed4b37ca6d83b5520782c213c9d99
Mario Oliva <90973494+Alkyz@users.noreply.github.com>
2023-11-20 13:00:27 -0500

    Update PageController.php to use PUT attribute function

--- nextad/lib/Controller/PageController.php
+++ nextad/lib/Controller/PageController.php
@@ -20,6 +20,7 @@ class PageController extends Controller {
         private LDAPService $ldapService,
         private ILogger $logger
     ) {
+        $this->request = $request;
         parent::__construct('nextad', $request);
     }
 
@@ -38,10 +39,10 @@ class PageController extends Controller {
 
     #[NoAdminRequired]
     #[NoCSRFRequired]
-    public function getUserAttributes(): JSONResponse {
+    public function getUserAttributes($uid): JSONResponse {
         $this->logger->info('checkLdapConnection called');
         try {
-            $message = $this->ldapService->connect();
+            $message = $this->ldapService->connect($uid);
             $this->logger->info('LDAP connection message: ' . $message);
             return new JSONResponse([
                 'status' => 'success',
@@ -53,6 +54,30 @@ class PageController extends Controller {
             return new JSONResponse([
                 'status' => 'error',
                 'message' => $e->getMessage(),
+            ], $e->getCode() === 0 ? 500 : $e->getCode());
+        }
+    }
+
+    #[NoAdminRequired]
+    #[NoCSRFRequired]
+    public function putUserAttributes($uid, IRequest $request): JSONResponse {
+        $this->logger->info('putUserAttributes called');
+        try {
+            $data = $this->request->getParams();
+            $message = $this->ldapService->updateUserAttributes($uid, $data);
+            $this->logger->info('LDAP update message: ' . $message);
+            return new JSONResponse([
+                'status' => 'success',
+                'message' => 'Attributes updated',
+                'debug' => $message,
+            ]);
+        } catch (\Exception $e) {
+            $this->logger->error('Error updating LDAP attributes: ' . $e->getMessage(), [
+                'exception' => $e]);
+            return new JSONResponse([
+                'status' => 'error',
+                'message' => 'Failed to Update LDAP Attributes',
+                'errorDetails' => $e->getMessage()
             ], $e->getCode() === 0 ? 500 : $e->getCode());
         }
     }
